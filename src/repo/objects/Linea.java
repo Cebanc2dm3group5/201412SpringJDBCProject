50315b9f0882e60496084b7a2a5ccaecdd8dea59
Gorriti <gorriti@hotmail.com>
2014-12-03 09:24:27 +0100

    create linea model

---
+++ src/repo/objects/Linea.java
@@ -0,0 +1,69 @@
+package repo.objects;
+
+public class Linea {
+	
+	//private static final long serialVersionUID = 1L;
+	private int albaran, linea, articulo, proveedor, cantidad, descuento;
+	private double precio;
+
+	public int getAlbaran() {
+		return albaran;
+	}
+
+	public void setAlbaran(int albaran) {
+		this.albaran = albaran;
+	}
+
+	public int getLinea() {
+		return linea;
+	}
+
+	public void setLinea(int linea) {
+		this.linea = linea;
+	}
+	
+	public int getArticulo() {
+		return articulo;
+	}
+
+	public void setArticulo(int articulo) {
+		this.articulo = articulo;
+	}
+	
+	public int getProveedor() {
+		return proveedor;
+	}
+
+	public void setProveedor(int proveedor) {
+		this.proveedor = proveedor;
+	}
+	
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	public int getDescuento() {
+		return descuento;
+	}
+
+	public void setDescuento(int descuento) {
+		this.descuento = descuento;
+	}
+	
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	/*public static long getSerialversionuid() {
+		return serialVersionUID;
+	}*/
+
+}
